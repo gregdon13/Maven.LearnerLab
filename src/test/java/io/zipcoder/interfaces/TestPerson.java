8bdf62e43cc9069910c000d4546add7deca4be1b
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-24 12:21:54 -0400

    Section 1 done

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,37 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
 
+    @Test
+    public void testConstructor () {
+        //Given
+        long expectedLong = 205;
+        String expectedStr = "John";
+        Person person = new Person(205, "John");
+
+        //When
+        long actualLong = person.getId();
+        String actualStr = person.getName();
+
+        //Then
+        Assert.assertEquals(expectedLong, actualLong);
+        Assert.assertEquals(expectedStr, actualStr);
+    }
+
+    @Test
+    public void testSetter() {
+        //Given
+        String expected = "Armando";
+        Person person = new Person(364, "");
+
+        //When
+        person.setName("Armando");
+        String actual = person.getName();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
